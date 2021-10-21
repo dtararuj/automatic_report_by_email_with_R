a3a63bce83770ea7125118903ba1ac849740d772
Dawid <tedi29571@gmail.com>
2021-10-21 18:17:23 +0200

    another example added

--- automatic_report_by_email.R
+++ automatic_report_by_email.R
@@ -19,12 +19,31 @@ OutMail=OutApp$CreateItem(0)
 
 #define recipient, subject and message text
 OutMail[["TO"]]= "dawidtararuj@10g.pl"
-OutMail[["Subject"]]="raport3"
-OutMail[["body"]]="raport w za³aczniku"
+OutMail[["Subject"]]="X report"
+OutMail[["body"]]="report attached below"
 
 #indicate the attachment, our report
 OutMail[["Attachments"]]$Add(file.path(getwd(),"raporttest.csv"))
 
 #3 Send an email when certain condition will be met 
 ifelse(report %>%  select(2) %>%  sum() > 499,OutMail$Send(),"email not sent")
+
+
+#4 Send an email to defined recipients
+
+recipients = c("dawidtararuj@10g.pl", "dawidtataruj@10g.pl","dawidtararuj2@10g.pl")
+
+if(report %>%  select(2) %>%  sum() > 499){
+  for (email in recipients){
+    OutApp<-COMCreate("Outlook.Application")
+    OutMail=OutApp$CreateItem(0)
+    OutMail[["TO"]]= email
+    OutMail[["Subject"]]="X report"
+    OutMail[["body"]]="Report attached below"
+    OutMail[["Attachments"]]$Add("C:/Users/Dawid/Desktop/raporttest.csv")
+    OutMail$Send()}
+  }else{
+    stop()
+  }
+
 
